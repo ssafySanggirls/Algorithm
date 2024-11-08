3bc3b2f8acc769b8513865cc52c1d10cd416e2b7
연주원 <50977497+joo1yeon@users.noreply.github.com>
2024-11-08 16:24:53 +0900

    Update: [1108] BJ 9252 LCS 2

---
+++ Backlog/jw/BOJ/G4_9252_LCS 2.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+
+        int[][] dp = new int[str1.length + 1][str2.length + 1];
+
+        for (int i = 1; i <= str1.length; i++) {
+            for (int j = 1; j <= str2.length; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        int i = str1.length;
+        int j = str2.length;
+        while (i > 0 && j > 0) {
+            if (dp[i][j] == dp[i - 1][j]) {
+                i--;
+            } else if (dp[i][j] == dp[i][j - 1]) {
+                j--;
+            } else {
+                stack.add(str1[i - 1]);
+                i--;
+                j--;
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.println(dp[str1.length][str2.length]);
+        if (dp[str1.length][str2.length] != 0) {
+            System.out.println(sb);
+        }
+    }
+}
